8f2b566e799ba98fd6e89779c2110cdfd3b5bf9e
AdeelAkram <adeelakram696@gmail.com>
2024-10-18 00:27:53 +0500

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,31 @@ import "../styles/argon-design-system-react.css";
 import "../styles/styles.css";
 import "../styles/vendor/font-awesome/css/font-awesome.min.css";
 import "../styles/vendor/nucleo/css/nucleo.css";
+import Script from "next/script";
 
+const GA_TRACKING_ID = 'G-TRXR54MY30'; 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
